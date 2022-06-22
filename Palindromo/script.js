29c81a74ad2da6d99501e8d2c0513dbc89d0b62b
bittoMazza <robertomazza99@gmail.com>
2022-06-22 13:42:24 +0200

    completed first exercise

--- Palindromo/script.js
+++ Palindromo/script.js
@@ -1,2 +1,32 @@
-const userWord;
+const userWord = prompt('Inserire una parola')
+let finalResult;
+finalResult = checkIfPalindrome(userWord);
+if(finalResult == true){
+    console.log('La parola è palindroma')
+}else{
+    console.log('La parola non è palindroma')
+}
 
+
+
+
+function checkIfPalindrome(word){
+    console.log(word)
+    let i= 0;
+    // IL CONTROLLO ARRIVA FINO A META PAROLA PERCHE LE PAROLE PALINDROME HANNO UNA META 
+    // UGUALE ALL'ALTRA MA CON LE LETTERE INVERSE
+    while( i < (word.length / 2)){
+        // PARAGONIAMO LA LETTERA ALLA POSIZIONE I CON QUELLA A FINE PAROLA - I
+        // IN MODO DA AVVICINARCI  A META PAROLA DA ENTRAMBE LE PARTI
+        if(word[i] == word[word.length - 1 -i])
+        {
+            result = true;
+            i++
+        }
+        else{
+            result = false
+            return result;
+        }
+    }
+    return result;
+}
